122e5e2607b9b244f5edc6cad95d25ef90b10fb0
Guilherme Oliveira <guilherme.061@gmail.com>
2017-12-11 16:42:21 -0200

    tentando

--- src/ea/evolutive/operators/fitness.hpp
+++ src/ea/evolutive/operators/fitness.hpp
@@ -1,6 +1,8 @@
 #ifndef __FITNESS__
 #define __FITNESS__
 
+#define BIT_DIV 2
+
 #include "ea/evolutive/evolutive.hpp"
 
 namespace Operators {
@@ -12,11 +14,27 @@ namespace Operators {
     };
 
     class BruteForceCollisionCheck : public FitnessFunction {
-        public:
-        virtual void calculateFitness(Individual& individual) {
-            hashwrapper * md5 = new md5wrapper();
-            hashwrapper * sha1 = new sha1wrapper();
+        private:
+            hashwrapper * md5;
+            hashwrapper * sha1;
+            std::string getHashFromInt(int n) {
+                std::string msg = md5->getHashFromString(std::to_string(n));
+                msg = msg.substr(0, (msg.size() / BIT_DIV));
+                std::string hash = sha1->getHashFromString(msg);
+                hash = hash.substr(0, (msg.size() / BIT_DIV));
 
+                return hash;
+            }
+        public:
+            BruteForceCollisionCheck() {
+                md5 = new md5wrapper();
+                sha1 = new sha1wrapper();
+            }
+            ~BruteForceCollisionCheck() {
+                delete md5;
+                delete sha1;
+            } 
+        void calculateFitness(Individual& individual) {
             // Searching for triple collisions (not optimized) 
 
             int m1 = individual.getM1(),
@@ -27,23 +45,24 @@ namespace Operators {
             std::vector<std::string> stored_hashes_first_round;
             std::vector<std::string> stored_hashes_second_round;
             for(int i = 0; i < m1; i++) {
-                stored_hashes_first_round.push_back(sha1->getHashFromString(md5->getHashFromString(std::to_string(i))));
+                stored_hashes_first_round.push_back(getHashFromInt(i));
             }
 
             for(int i = m1; i < (m1+m2); i++) {
                 bool isOnFirstRound = false;
                 for(int j = 0; j < stored_hashes_first_round.size(); j++) {
-                    if(sha1->getHashFromString(md5->getHashFromString(std::to_string(i))) == stored_hashes_first_round[j]) {
+                    if(getHashFromInt(i) == stored_hashes_first_round[j]) {
                         isOnFirstRound = true;
+                        std::cout << "Collision on Second Round" << std::endl;
                     }
                 }
-                if(isOnFirstRound) stored_hashes_second_round.push_back(sha1->getHashFromString(md5->getHashFromString(std::to_string(i))));
+                if(isOnFirstRound) stored_hashes_second_round.push_back(getHashFromInt(i));
             }
 
             for(int i = (m1+m2); i < (m1+m2+m3); i++) {
                 bool isOnSecondRound = false;
                 for(int j = 0; j < stored_hashes_second_round.size(); j++) {
-                    if(sha1->getHashFromString(md5->getHashFromString(std::to_string(i))) == stored_hashes_second_round[j]) {
+                    if(getHashFromInt(i) == stored_hashes_second_round[j]) {
                         isOnSecondRound = true;
                     }
                 }
@@ -51,9 +70,6 @@ namespace Operators {
             }
 
             individual.setFitness(collisions);
-
-            delete md5;
-            delete sha1;
         }
     };
 }
